c0a8d8c639ced8aaff6c34e7e08b5d6769f4c12d
Fraloha <infofrancesco20@gmail.com>
2023-05-19 10:08:44 +0200

    Created HomeView

---
+++ src/main/java/brickbreaker/view/HomeView.java
@@ -0,0 +1,35 @@
+package brickbreaker.view;
+
+import brickbreaker.controllers.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class HomeView extends ViewImpl {
+
+    @FXML
+    private ImageView title;
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    private Button endlessPlayButton;
+
+    @FXML
+    private Button globalRanksButton;
+
+    @FXML
+    private Button rulesButton;
+
+    public HomeView(final Controller controllerToAttach) {
+        super(controllerToAttach);
+    }
+    
+    @Override
+    public void init() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'init'");
+    }
+    
+}

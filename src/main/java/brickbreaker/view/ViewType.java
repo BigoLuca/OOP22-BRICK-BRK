5b2f5c259320ea2cf049f1486f4ff5d331aae363
Fraloha <infofrancesco20@gmail.com>
2023-05-29 11:39:26 +0200

    Added some views.

--- src/main/java/brickbreaker/view/ViewType.java
+++ src/main/java/brickbreaker/view/ViewType.java
@@ -1,14 +1,24 @@
 package brickbreaker.view;
 
 import brickbreaker.controllers.Controller;
+import brickbreaker.controllers.ControllerImpl;
+import brickbreaker.controllers.HomeController;
+import brickbreaker.controllers.RankController;
+import brickbreaker.controllers.SetUpController;
 import brickbreaker.controllers.state.LevelControllerImpl;
 
 public enum ViewType {
     HOME("HomeView", new HomeController()),
 
+    LEVEL("LevelMenuView", new ControllerImpl()),
+
+    ENDLESS("EndlessMenuView", new ControllerImpl()),
+
     RANK("RankView", new RankController()),
 
-    MATCH("MatchView", new LevelControllerImpl());
+    MATCH("MatchView", new LevelControllerImpl()),
+
+    SET_UP("SetUp", new SetUpController());
 
     private static final String DIRECTORY = "viewStyle/";
     private static final String FORMAT = ".fxml";

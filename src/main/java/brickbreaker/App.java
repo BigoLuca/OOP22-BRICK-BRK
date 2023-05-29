c2c450546a06b217c5de29828672933d554bd919
Fraloha <infofrancesco20@gmail.com>
2023-05-29 11:40:20 +0200

    Modified.

--- src/main/java/brickbreaker/App.java
+++ src/main/java/brickbreaker/App.java
@@ -1,19 +1,24 @@
 package brickbreaker;
 
 import brickbreaker.view.JavaFXApp;
+import brickbreaker.view.ViewSwitcher;
+import brickbreaker.view.ViewType;
 import javafx.application.Application;
+import javafx.stage.Stage;
 
 /**
  * Main project class.
  */
-public final class App {
+public final class App extends Application {
 
-    private App() { }
-    /**
-     * Entry point.
-     * @param args command line args
-     */
-    public static void main(final String[] args) throws Exception {
-        Application.launch(JavaFXApp.class, args);
+    public static void main(final String[] args) {
+        launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        ViewSwitcher.getInstance().switchView(primaryStage, ViewType.HOME, );
+    }
+
+    
 }

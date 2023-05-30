f9936deb2ef77d8dbb841580f7c549a19e592332
Luca Bighini <bighini.luca@gmail.com>
2023-05-30 12:51:43 +0200

    Create ModelController.java

---
+++ src/main/java/brickbreaker/controllers/ModelController.java
@@ -0,0 +1,35 @@
+package brickbreaker.controllers;
+
+import brickbreaker.controllers.state.ErrorListener;
+import brickbreaker.controllers.state.LevelController;
+import brickbreaker.model.GameModel;
+import brickbreaker.model.GameModelImpl;
+
+public class ModelController {
+    
+    private UserController userController;
+    private RankController rankController;
+    private LevelController levelController;
+    private ErrorListener errListener;
+    private GameModel model;
+
+    public ModelController() {
+        this.userController = new UserController();
+        this.rankController = new RankController();
+        this.errListener = new ErrorListener();
+        this.model = new GameModelImpl();
+    }
+
+    public UserController getUserController() { return this.userController; }
+
+    public RankController getRankController() { return this.rankController; }
+
+    public LevelController getLevelController() { return this.levelController; }
+
+    public void setLevelController(LevelController lc) { this.levelController = lc; }
+
+    public ErrorListener getErrorListener() { return this.errListener; }
+
+    public GameModel getModel() { return this.model; }
+
+}

22481e0f0819b7406d706a20dd133bba6e883399
Luca Bighini <bighini.luca@gmail.com>
2023-05-30 12:52:18 +0200

    Update Controller

--- src/main/java/brickbreaker/controllers/Controller.java
+++ src/main/java/brickbreaker/controllers/Controller.java
@@ -1,39 +1,145 @@
 package brickbreaker.controllers;
 
-import brickbreaker.model.GameModel;
-import brickbreaker.view.View;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-/**
- * This interface defines the game controller: the interaction between model and view.
- * 
- * @author Agostinelli Francesco
- */
-public interface Controller {
+import brickbreaker.common.State;
+import brickbreaker.controllers.state.LevelControllerImpl;
+import brickbreaker.model.Level;
+import brickbreaker.model.rank.GameRank;
+import brickbreaker.model.rank.Rank;
+import brickbreaker.model.user.User;
 
-    /**
-     * Method to set the Model.
-     * @param modelToAttach
-     */
-    void setModel(GameModel modelToAttach);
+public class Controller {
 
-    /**
-     * @return the model as GameState class
-     */
-    GameModel getModel();
+    private ModelController modelController;
+    //private ViewController viewController;
+    private boolean haveWin;
+    private Thread gameLoop;
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
 
-    /**
-     * Method to set the View.
-     * @param viewToAttach
-     */
-    void setView(View viewToAttach);
+    private User user = new User("Pippo");
 
-    /**
-     * @return the view
-     */
-    View getView();
+    public Controller() {
+        this.modelController = new ModelController();
+    }
 
-    /**
-     * Abstract method to initialize the game.
-     */
-    void init();
+    public void setUser(final String username) {
+        this.user = this.modelController.getUserController().getUser(username);
+    }
+
+    public void createEndless() {
+        lock.lock();
+        Rank r = new GameRank("endless.json");
+        final Integer maxIteration = 10;
+        Integer totalScore = 0;
+
+        do {
+            haveWin = false;
+            Integer it = 0;
+            Level level = null;
+            do {
+                modelController.getErrorListener().getErrorList().clear();
+                level = modelController.getModel().getRandomLevel();
+                it++;
+            } while (modelController.getErrorListener().getErrorPresent() && it < maxIteration);
+
+            if (it < maxIteration) {
+                LevelControllerImpl levCon = new LevelControllerImpl(level);
+                modelController.setLevelController(levCon);
+                gameLoop = new Thread(levCon);
+
+                try {
+                    condition.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+                //dai il comando alla view:  
+                // this.viewController.showLevel(level).show(); + while fino a quando non clicca play + ritorna l'angolo della pallina
+                System.out.println("parto con il GameLoop");
+                
+                gameLoop.start();
+                try {
+                    // Main thread wait the execution of "gameLoop" thread
+                    gameLoop.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                totalScore += levCon.getScore();
+                if(levCon.getState().equals(State.WIN)) {
+                    haveWin = true;
+                }
+            } else {
+                // mostra errore caricamento
+            }
+        } while (haveWin);
+        r.addRank(user.getName(), totalScore);
+    }
+
+    public synchronized void createLevel(final Integer id) {
+        lock.lock();
+        Rank r = new GameRank("levels.json");
+        modelController.getErrorListener().getErrorList().clear();
+        Level level = modelController.getModel().getLevel(id);
+
+        if(!modelController.getErrorListener().getErrorPresent()){
+            LevelControllerImpl levCon = new LevelControllerImpl(level);
+            modelController.setLevelController(levCon);
+            gameLoop = new Thread(levCon);
+
+            try {
+                condition.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+
+            // this.viewController.showLevel(level).show(); + while fino a quando non clicca play + ritorna l'angolo della pallina
+            
+            gameLoop.start();
+            try {
+                // Main thread wait the execution of "gameLoop" thread
+                gameLoop.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if(levCon.getState().equals(State.WIN)) { 
+                r.addRank(
+                    user.getName(),
+                    r.getPlayerScore(user.getName()) + levCon.getScore());
+                //set level score
+            }
+        } else {
+            // mostra errore caricamento
+        }
+    }
+
+    public void play() {
+        lock.lock();
+        try {
+            condition.signal();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void pause() {
+        modelController.getLevelController().pauseGame();
+    }
+
+    public void resume() throws InterruptedException {
+        modelController.getLevelController().resumeGame();
+    }
+
+    public void quit() {
+        modelController.getLevelController().quitGame();
+    }
 }

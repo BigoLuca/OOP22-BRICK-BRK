8c6d67062224a71cc3385123d600f853ff46f697
Luca Bighini <bighini.luca@gmail.com>
2023-06-03 00:17:03 +0200

    Update RankController.java

--- src/main/java/brickbreaker/controllers/RankController.java
+++ src/main/java/brickbreaker/controllers/RankController.java
@@ -1,31 +1,18 @@
 package brickbreaker.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 import brickbreaker.ResourceLoader;
-import brickbreaker.model.rank.GameRank;
-import brickbreaker.model.rank.Rank;
 
 public class RankController {
 
-    private List<Rank> ranks;
-
-    public RankController() {
-        ranks = new ArrayList<>();
-    }
-
-    public List<Rank> getListOfRanks() {
-        return ResourceLoader.getInstance().getMapsNames()
-                .stream()
-                .map(e -> new GameRank(e))
-                .collect(Collectors.toList());
-    }
-
-    public Rank getRank(final String name) {
-        return ranks.stream()
-                .filter(e -> e.getFileName().equals(name))
-                .findFirst().orElseThrow();
+    /**
+     * Method to get the current rank.
+     * @param fileName
+     * @param level
+     * @return a map of user names and their ranks
+     */
+    public Map<String, Integer> getRank(final String fileName, final Integer level) {
+        return ResourceLoader.getInstance().getRank(fileName, level);
     }
 }

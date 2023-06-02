1bae44d0441db3a6ef43e18a001f3633be47e10b
Luca Bighini <bighini.luca@gmail.com>
2023-06-02 18:16:43 +0200

    Update UserController.java

--- src/main/java/brickbreaker/controllers/UserController.java
+++ src/main/java/brickbreaker/controllers/UserController.java
@@ -1,6 +1,5 @@
 package brickbreaker.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,28 +7,22 @@ import brickbreaker.ResourceLoader;
 import brickbreaker.model.user.User;
 
 public class UserController {
-    
-    private List<User> users;
 
-    public UserController() {
-        users = new ArrayList<>();
-    }
+    public UserController() {}
 
     public List<String> getUsersName() {
-        return users.stream().map(User::getName).collect(Collectors.toList());
+        return ResourceLoader.getInstance().getUsers().stream().map(User::getName).collect(Collectors.toList());
     }
 
     public User getUser(final String username) {
-        return users.stream().filter(user -> user.getName().equals(username)).findFirst().get();
+        return ResourceLoader.getInstance().getUsers().stream().filter(user -> user.getName().equals(username)).findFirst().get();
     }
 
     public void addUser(final String username) {
         ResourceLoader.getInstance().addUser(username);
-        this.users = ResourceLoader.getInstance().getUsers();
     }
 
     public void removeUser(final String username) {
         ResourceLoader.getInstance().removeUser(username);
-        this.users = ResourceLoader.getInstance().getUsers();
     }
 }

bed225e60781141ee0bc286c5c73494175073d4a
Luca Bighini <bighini.luca@gmail.com>
2023-06-12 00:46:57 +0200

    Update App.java
    to show on the top border

--- src/main/java/brickbreaker/App.java
+++ src/main/java/brickbreaker/App.java
@@ -3,6 +3,8 @@ package brickbreaker;
 import brickbreaker.view.ViewSwitcher;
 import brickbreaker.view.ViewType;
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 /**
@@ -16,14 +18,27 @@ public class App extends Application{
      */
     @Override
     public void start(Stage primaryStage) throws Exception {
-        try{
+        try {
             ResourceLoader.getInstance().start();
         } catch (Exception e) {
             System.out.println("Error while loading resources");
         }
-
         
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+        double centerX = bounds.getMaxX() / 3.5;
+        double centerY = bounds.getMinY() + 20;
+        
+        primaryStage.setX(centerX);
+        primaryStage.setY(centerY);
+
+        primaryStage.setOnCloseRequest(event -> {
+            System.exit(0);
+        });
+
         ViewSwitcher.getInstance().switchView(primaryStage, ViewType.SETUP);
+
+        primaryStage.show();
     }
 
 }

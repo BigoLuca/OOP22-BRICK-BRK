b7051e4fb782ceef3c3c94c80deadb688e3a0326
Luca Bighini <bighini.luca@gmail.com>
2023-06-12 11:38:38 +0200

    Update GameImages.java

--- src/main/java/brickbreaker/common/GameImages.java
+++ src/main/java/brickbreaker/common/GameImages.java
@@ -1,65 +1,94 @@
 package brickbreaker.common;
 
-
 import javafx.scene.image.Image;
 
+/**
+ * Game Images enum with name files.
+ */
 public enum GameImages {
 
+    /** Forest landscape. */
     FOREST_LANDSCAPE("forest.png"),
 
+    /** Space landscape. */
     SPACE_LANDSCAPE("background.jpg"),
 
+    /** Night sky. */
     NIGHT_SKY("night-sky.jpg"),
 
+    /** Sea landscape. */
     SEA_LANDSCAPE("nickname.png"),
 
+    /** City landscape. */
     CITY_LANDSCAPE("City.png"),
 
+    /** Game icon. */
     GAME_ICON("nickname.png"),
 
+    /** Easy difficulty. */
     EASY_DIFFICULTY("easy.png"),
 
+    /** Medium difficulty. */
     MEDIUM_DIFFICULTY("medium.png"),
 
+    /** Hard difficulty. */
     HARD_DIFFICULTY("hard.png"),
 
+    /** Mix difficulty. */
     MIX_DIFFICULTY("mix.png"),
 
+    /** Nickname label. */
     NICKNAME_LABEL("nickname.png"),
-    
+
+    /** Type your name label */
     TYPE_YOUR_NAME_LABEL("typeYourName.png"),
 
+    /** Pick a level. */
     PICK_A_LEVEL("pickALevel.png"),
 
+    /** Next. */
     NEXT("next.png"),
 
+    /** Previous. */
     PREVIOUS("previous.png"),
 
+    /** Not loaded item. */
     NOT_LOADED_ITEM("nickname.png"),
-    
+
+    /** Title. */
     TITLE("Title.png"),
 
+    /** Endless mode choice. */
     ENDLESS_MODE_CHOICE("endless.png"),
 
+    /** Leevls label. */
     LEVELS_LABEL("levels.png"),
 
+    /** Levels mode choice. */
     LEVELS_MODE_CHOICE("start.png"),
 
+    /** Leaderboard choice. */
     LEADERBOARD_CHOICE("leaderboards.png"),
 
+    /** Up arrow. */
     UP_ARROW("upArrow.png"),
 
+    /** Down arrow. */
     DOWN_ARROW("downArrow.png"),
 
+    /** Right arrow. */
     RIGHT_ARROW("rightArrow.png"),
 
+    /** Left arrow. */
     LEFT_ARROW("leftArrow.png"),
 
+    /** Player lost. */
     PLAYER_LOST("youLost.png"),
-    
+
+    /** Player win. */
     PLAYER_WIN("youWin.png");
 
-    private static final String RES_PATH = "images/";
+    private final String RES_PATH = "images/";
     private String fileName;
     private Image image;
 
@@ -67,14 +96,25 @@ public enum GameImages {
         this.fileName = fileName;
     }
 
+    /**
+     * Mathod to set the file Image.
+     * @param image
+     */
     public void setImage(final Image image) {
         this.image = image;
     }
 
+    /**
+     * Method to get the Image.
+     * @return
+     */
     public Image getImage() {
         return this.image;
     }
 
+    /**
+     * Method to get the absolute path.
+     */
     public String getFilePath() {
         return RES_PATH + this.fileName;
     }

dbe1714b83e92f057639b030b36fe0bacf606c30
Tella <tellarinipietro@gmail.com>
2023-06-17 00:43:43 +0200

    Update UserController.java

--- src/main/java/brickbreaker/controllers/UserController.java
+++ src/main/java/brickbreaker/controllers/UserController.java
@@ -15,17 +15,27 @@ import brickbreaker.model.user.User;
 public class UserController {
 
     private static final int MAX_PLAYER = 5;
+    private static final String USER_FILE = "users/user.json";
     private final List<User> users;
 
     /**
      * Empy UserController constructor.
      */
     public UserController() {
-        this.users = LoadJson.load(new TypeToken<List<User>>(){}.getType(), "users/user.json");
+        this.users = LoadJson.load(new TypeToken<List<User>>() {
+        }.getType(), USER_FILE);
+    }
+
+    /**
+     * Method to save the users.
+     */
+    public void saveUsers() {
+        LoadJson.save(this.users, USER_FILE);
     }
 
     /**
      * Methdo to get all the name of the user saved.
+     * 
      * @return the List of name
      */
     public List<String> getUsersName() {
@@ -34,33 +44,36 @@ public class UserController {
 
     /**
      * Method to get a user passing the name.
+     * 
      * @param username
      * @return a User
      */
     public User getUser(final String username) {
-        return ResourceLoader.getInstance().getUsers().stream().filter(user -> user.getName().equals(username))
-                .findFirst().get();
+        return this.users.stream().filter(u -> u.getName().equals(username)).findFirst().orElse(null);
     }
 
     /**
      * Method to add a new user to json file.
+     * 
      * @param newUser
      */
     public void addUser(final User newUser) {
         this.users.add(newUser);
-        LoadJson.write(newUser, "users/user.json");
+        saveUsers();
     }
 
     /**
      * Method to remove user and his ranks from json file.
+     * 
      * @param username
      */
     public void removeUser(final String username) {
-        ResourceLoader.getInstance().removeUser(username);
+        this.users.removeIf(u -> u.getName().equals(username));
     }
 
     /**
      * Method to chek the number of user.
+     * 
      * @return true if less than MAX_PLAYER
      */
     public boolean isMaxUser() {

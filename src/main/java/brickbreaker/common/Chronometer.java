2fb7281b3dc1d5f5443ab9fc3975bf04e1e5cb62
Luca Bighini <bighini.luca@gmail.com>
2023-06-16 12:13:08 +0200

    Update Chronometer.java
    Remove shynchronized

--- src/main/java/brickbreaker/common/Chronometer.java
+++ src/main/java/brickbreaker/common/Chronometer.java
@@ -22,7 +22,7 @@ public class Chronometer extends Thread {
     /**
      * @return the time elapsed from the start in seconds
      */
-    public synchronized Integer getElapsedTime() {
+    public Integer getElapsedTime() {
         return this.time / 10;
     }
 

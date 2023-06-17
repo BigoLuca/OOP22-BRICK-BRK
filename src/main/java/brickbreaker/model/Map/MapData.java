ca6c58f2d7ecc2fd3dc5cec57e0a8ddd9009c17d
Tella <tellarinipietro@gmail.com>
2023-06-17 04:22:01 +0200

    Class describing a map

--- src/main/java/brickbreaker/model/Map/MapData.java
+++ src/main/java/brickbreaker/model/Map/MapData.java
@@ -1,13 +1,37 @@
-package brickbreaker.model.Map;
+package brickbreaker.model.map;
 
 import java.util.List;
 
+import brickbreaker.common.Difficulty;
+import brickbreaker.common.GameImages;
+
 public class MapData {
+
+    public static final int MAP_ROWS_FILE_FORMAT = 6;
+    public static final int MAP_COLUMNS_FILE_FORMAT = 6;
+
     private int index;
     private String landscape;
-    private List<List<Integer>> map;
-    private String difficulty;
+    private List<Integer> map;
+    private Integer difficulty;
     private String name;
+
+    /**
+     * Map info constructor.
+     * @param i
+     * @param b
+     * @param d
+     * @param name
+     * @param landscape
+     */
+    public MapData(final Integer i, final List<Integer> b, final Integer d, final String name, final String landscape) {
+        this.index = i;
+        this.map = b;
+        this.difficulty = d;
+        this.name = name;
+        this.landscape = landscape;
+        return;
+    }
 
     public int getIndex() {
         return index;
@@ -17,28 +41,28 @@ public class MapData {
         this.index = index;
     }
 
-    public String getLandscape() {
-        return landscape;
+    public GameImages getLandscape() {
+        return GameImages.valueOf(landscape);
     }
 
     public void setLandscape(String landscape) {
         this.landscape = landscape;
     }
 
-    public List<List<Integer>> getMap() {
+    public List<Integer> getMap() {
         return map;
     }
 
-    public void setMap(List<List<Integer>> map) {
+    public void setMap(List<Integer> map) {
         this.map = map;
     }
 
-    public String getDifficulty() {
-        return difficulty;
+    public Difficulty getDifficulty() {
+        return Difficulty.values()[difficulty];
     }
 
-    public void setDifficulty(String difficulty) {
-        this.difficulty = difficulty;
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty.ordinal();
     }
 
     public String getName() {
